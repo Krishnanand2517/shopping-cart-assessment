ca910e1f51cc74a5d1ed8b128467dd8d126b4ef7
Krishnanand Yadav <krishnanand2517@gmail.com>
2025-11-11 02:53:24 +0530

    style: Design the product listing page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-background-950 p-8 md:p-16 lg:px-32 text-text-secondary">
+    <div className="min-h-screen bg-background-950 px-8 md:px-16 lg:px-32 text-text-secondary relative">
       <div className="max-w-7xl mx-auto">
         <Outlet />
       </div>
